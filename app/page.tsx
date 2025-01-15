095721c17fd2bd1b6ddc2208ebb09af77b7a31a3
yourhero <141227376+chatgptsuper@users.noreply.github.com>
2025-01-16 03:44:22 +0800

    重新进行修改12

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 import { defaultLanguage } from '../config/languages'
 
-export default function Home() {
+export default function RootPage() {
   redirect(`/${defaultLanguage}`)
 }
